53532d2c473bea2394ca82499c42ced15ecbb83e
Enrique Gonzalez <enriikke@gmail.com>
2013-08-03 23:27:48 -0400

    Adding some comments to the code

--- suave.js
+++ suave.js
@@ -1,5 +1,21 @@
 
-
+/*
+ *      Suave makes it easier to apply smooth vertical scrolling to all of the same-page
+ *		links on your website. Simply pass in all anchor tags and you are set. You can 
+ *		also select specific classes or IDs if you don't need it on all links. I'm working
+ *		on adding support for horizontal scrolling and also to make Suave more 
+ *		customizable. Stay tuned
+ *
+ *      Suave requires jQuery to work.
+ *
+ *
+ *      by Enrique Gonzalez (Enriikke)
+ *      helloimenrique.com
+ *      enrique@chiibo.com
+ *
+ *      enjoy!
+ *
+ */
 (function ($) {
 	
 	$.fn.suave = function(options) {
