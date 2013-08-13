521dbaac275324d856a8456cafb0fc195aee62a6
Enrique Gonzalez <enriikke@gmail.com>
2013-08-12 22:16:25 -0400

    Started configuration for horizontal scrolling.

--- suave.js
+++ suave.js
@@ -1,6 +1,6 @@
 
 /*
- *      Suave makes it easier to apply smooth vertical scrolling to all of the same-page
+ *  Suave makes it easier to apply smooth vertical scrolling to all of the same-page
  *	links on your website. Simply pass in all anchor tags and you are set. You can 
  *	also select specific classes or IDs if you don't need it on all links. I'm working
  *	on adding support for horizontal scrolling and also to make Suave more 
@@ -23,8 +23,9 @@
 		// Customizable settings for Suave
 		var settings = $.extend({
 
-			speed: 		400, // How fast should the animation be
-			displayHash: 	false // Display the link hash in the URL bar
+			speed: 				400, // How fast should the animation be
+			displayURL: 		false, // Display the link hash in the URL bar
+			horizontalScroll: 	false // Allow for horizontal scrolling
 
 		}, options);
 
@@ -32,7 +33,7 @@
 
 		/****************************************************************************************************
         		NICE UTIL FUNCTIONS FOR VAPE TO WORK 
-        	*****************************************************************************************************/
+        *****************************************************************************************************/
 
 		// TODO: Need to filterout the target
 		var getTarget = function(anchor) {
@@ -56,8 +57,8 @@
 
 
 		/****************************************************************************************************
-                        THE MAGIC HAPPENS HERE 
-                *****************************************************************************************************/
+                THE MAGIC HAPPENS HERE 
+        *****************************************************************************************************/
                 
 		return this.each(function() {
 			var target = getTarget(this);
@@ -68,7 +69,7 @@
 
 				var offset = $(target).offset().top;
 				$('html, body').animate({scrollTop: offset}, settings.speed, function() {
-					if(settings.displayHash) setHash(target);
+					if(settings.displayURL) setHash(target);
 				});
 			});
 		});
